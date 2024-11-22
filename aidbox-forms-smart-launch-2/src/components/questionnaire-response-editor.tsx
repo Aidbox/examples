98683c60da9bcd6d11cc2131f4601fe251115a80
Olim Saidov <olim.mail@gmail.com>
2024-11-22 20:34:50 +0500

    Add edit questionnaire response page

---
+++ aidbox-forms-smart-launch-2/src/components/questionnaire-response-editor.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { FormsBuilder } from "@/components/forms-builder";
+import { Questionnaire, QuestionnaireResponse } from "fhir/r4";
+import { Suspense, useTransition } from "react";
+import { Spinner } from "@/components/spinner";
+import { FormsRenderer } from "@/components/forms-renderer";
+
+interface QuestionnaireResponseEditorProps {
+  questionnaire: Questionnaire;
+  questionnaireResponse: QuestionnaireResponse;
+  onSaveAction: (
+    questionnaireResponse: QuestionnaireResponse,
+  ) => Promise<QuestionnaireResponse>;
+}
+
+export function QuestionnaireResponseEditor({
+  questionnaire,
+  questionnaireResponse,
+  onSaveAction,
+}: QuestionnaireResponseEditorProps) {
+  const [, startTransition] = useTransition();
+
+  return (
+    <Suspense fallback={<Spinner expand="true" />}>
+      <FormsRenderer
+        questionnaire={questionnaire}
+        questionnaireResponse={questionnaireResponse}
+        onChange={(updatedQuestionnaireResponse) => {
+          startTransition(async () => {
+            try {
+              await onSaveAction(updatedQuestionnaireResponse);
+            } catch (error) {
+              console.error("Failed to save questionnaire response:", error);
+            }
+          });
+        }}
+      />
+    </Suspense>
+  );
+}
