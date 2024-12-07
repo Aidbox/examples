9c346f4713eb6938fb68d3aeddca2299470d149b
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2024-12-07 12:53:52 +0100

    WIP: refactoring, add poetry project file, add make file, add .env.tpl

---
+++ aidbox-with-python-sdk/README.md
@@ -0,0 +1,9 @@
+# Python SDK Example
+
+``` shell
+cp .env.tpt .env
+# manually add license to .env
+docker compose up
+poetry shell
+python3 main.py
+```
--- aidbox-with-python-sdk/main.py
+++ aidbox-with-python-sdk/main.py
@@ -1,90 +1,131 @@
-from fhirpy import SyncFHIRClient
-from typing import List, Optional, Literal, Annotated
-from fhirpy.base.resource_protocol import ResourceProtocol
-from pydantic import BaseModel as BaseModel_, Field, Extra
+# ruff: noqa: E402
+
 from datetime import datetime
 
-# Models
-class BaseModel(BaseModel_):
-    class Config:
-        validate_assignment = True
-        allow_population_by_field_name = True
 
-    def dict(self, *args, **kwargs):
-        by_alias = kwargs.pop('by_alias', True)
-        return super().dict(*args, **kwargs, by_alias=by_alias)
-
-
-class HumanName(BaseModel):
-    use: Optional[str] = None
-    text: Optional[str] = None
-    given: Optional[List[str]] = None
-    family: Optional[str] = None
-    prefix: Optional[List[str]] = None
-    suffix: Optional[List[str]] = None
-
-
-class Patient(BaseModel):
-    resourceType: str = 'Patient'
-    id: Optional[str] = None
-    gender: Optional[str] = None
-    name: Optional[List[HumanName]] = None
-    active: Optional[bool] = None
-
-client = SyncFHIRClient(
-    url="http://localhost:8888/",
-    authorization="Basic cm9vdDpzZWNyZXQ=",
-    # dump_resource=BaseModel.model_dump
-)
-
-newpt = Patient(
-    name=[HumanName(given=['rabrab'], family='Bababa')],
-    active=True,
-    gender='female'
-)
-
-def csv_to_dict(file_path):
-    with open(file_path, mode='r') as file:
+def read_csv_as_dict(file_path):
+    with open(file_path, mode="r") as file:
         lines = file.readlines()
-        keys = lines[0].strip().split(',')
+        keys = lines[0].strip().split(",")
         data = lines[1:]
         result = []
         for line in data:
-            values = line.strip().split(',')
+            values = line.strip().split(",")
             result.append(dict(zip(keys, values)))
         return result
 
-def dict_to_csv(data, file_path):
-    with open(file_path, mode='w') as file:
+
+def write_dict_as_csv(data, file_path):
+    with open(file_path, mode="w") as file:
         columns = data[0].keys()
         rows = []
         for item in data:
-            rows.append(','.join([str(item[column]) for column in columns]))
-        file.write(','.join(columns) + '\n')
-        file.write('\n'.join(rows))
+            rows.append(",".join([str(item[column]) for column in columns]))
+        file.write(",".join(columns) + "\n")
+        file.write("\n".join(rows))
+
+
+def now():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S+00:00")
+
+
+#####################################################################
+# Use <https://github.com/beda-software/fhir-py>. Only Runtime
+
+
+from fhirpy import SyncFHIRClient
 
 
 def import_data(client, file_path):
-    raw_data = csv_to_dict(file_path)
-    now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S+00:00')
+    raw_data = read_csv_as_dict(file_path)
     for item in raw_data:
-        patient_to_update = client.resource("Patient",
-            name=[{'given': [item['given']], 'family': item['family']}],
-            gender=item['gender'],
+        patient_to_update = client.resource(
+            "Patient",
+            name=[{"given": [item["given"]], "family": item["family"]}],
+            gender=item["gender"],
         )
-        patient, is_created = client.resources("Patient").search(given=item['given'], family=item['family']).update(patient_to_update)
-        patient_id = dict(patient)['id']
-        observation = client.resource("Observation",
-            status='final',
-            code={'text': 'Body weight'},
-            subject={'reference': f'Patient/{patient_id}'},
-            valueQuantity={'value':int(item['weight']), 'unit': 'kg'},
-            effectiveDateTime=now,
+        patient, is_created = (
+            client.resources("Patient")
+            .search(given=item["given"], family=item["family"])
+            .update(patient_to_update)
+        )
+        patient_id = dict(patient)["id"]
+        print(
+            f"Patient {item [ 'given']} {item['family']} {'created' if is_created else 'updated'}. Id: {patient_id}"
+        )
+        observation = client.resource(
+            "Observation",
+            status="final",
+            code={"text": "Body weight"},
+            subject={"reference": f"Patient/{patient_id}"},
+            valueQuantity={"value": int(item["weight"]), "unit": "kg"},
+            effectiveDateTime=now(),
+        )
+        observation.save()
+        print(
+            f"Obsetvation for {item['given']} {item['family']} created. Id: {observation.id}"
         )
 
-import_data(client, 'patients.csv')
 
-        # find id (new patient or existing)
+client = SyncFHIRClient(
+    url="http://localhost:8888/", authorization="Basic cm9vdDpzZWNyZXQ="
+)
+
+# client.resource("Patient", name=[{"given": ["given"], "family": "family"}]).save()
+
+import_data(client, "data/patients.csv")
+
+
+#####################################################################
+# Use <https://github.com/beda-software/fhir-py>. With generated
+# resource classes
+
+
+# from fhirpy.base.resource_protocol import ResourceProtocol
+# from pydantic import BaseModel as BaseModel_, Field, Extra
+# from typing import List, Optional, Literal, Annotated
+
+
+# # Models
+# class BaseModel(BaseModel_):
+#     class Config:
+#         validate_assignment = True
+#         allow_population_by_field_name = True
+
+#     def dict(self, *args, **kwargs):
+#         by_alias = kwargs.pop("by_alias", True)
+#         return super().dict(*args, **kwargs, by_alias=by_alias)
+
+
+# class HumanName(BaseModel):
+#     use: Optional[str] = None
+#     text: Optional[str] = None
+#     given: Optional[List[str]] = None
+#     family: Optional[str] = None
+#     prefix: Optional[List[str]] = None
+#     suffix: Optional[List[str]] = None
+
+
+# class Patient(BaseModel):
+#     resourceType: str = "Patient"
+#     id: Optional[str] = None
+#     gender: Optional[str] = None
+#     name: Optional[List[HumanName]] = None
+#     active: Optional[bool] = None
+
+
+# client = SyncFHIRClient(
+#     url="http://localhost:8888/",
+#     authorization="Basic cm9vdDpzZWNyZXQ=",
+#     # dump_resource=BaseModel.model_dump
+# )
+
+# newpt = Patient(
+#     name=[HumanName(given=["rabrab"], family="Bababa")], active=True, gender="female"
+# )
+
+
+# find id (new patient or existing)
 # patient = client.resources('Patient').search(name=f"{item['first_name']} {item['last_name']}").first()
 
 # patient = client.resource('patient', )
@@ -103,8 +144,6 @@ import_data(client, 'patients.csv')
 #         1. Python SDK Runtime + FHIR Schema Codegen.
 #     - In process we should use something like PyCharm and note autocompletion and typechecking warnings.
 #     - Make comparison table between different SDK adoption levels. It should show to us sailing/growing points.
-
-
 
 
 # client.create(newpt)
