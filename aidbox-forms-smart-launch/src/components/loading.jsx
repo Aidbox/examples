85d10931b0ed85063e8c0c98a0cdced6f9753883
Olim Saidov <olim.mail@gmail.com>
2024-12-13 16:07:36 +0500

    Remove obsolete smart app

--- aidbox-forms-smart-launch/src/components/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/spinner.jsx";
-
-export const Loading = () => {
-  return (
-    <div className="flex-1 grid place-items-center">
-      <Spinner width="80" />
-    </div>
-  );
-};
--- aidbox-forms-smart-launch/src/pages/questionnaire-editor.jsx
+++
@@ -1,8 +0,0 @@
-import { QuestionnaireBuilder } from "@/components/questionnaire-builder.jsx";
-import { useParams } from "react-router-dom";
-
-export const QuestionnaireEditor = () => {
-  const { id } = useParams();
-
-  return <QuestionnaireBuilder id={id} />;
-};
