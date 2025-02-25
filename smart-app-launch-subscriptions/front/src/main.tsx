eb7f9e9d72e5147b86dc54e5976d23dba1a9c325
Igor Kisliakov <igor_kislyakov@yahoo.com>
2025-02-25 13:51:23 +0300

    smart app subscriptions - front - added iframe

--- smart-app-launch-subscriptions/front/src/main.tsx
+++ smart-app-launch-subscriptions/front/src/main.tsx
@@ -9,7 +9,43 @@ export const init = (containerId: string, config: SmartAppLaunchSubscriptionsCon
   }
 
   const container = document.getElementById(containerId)
-  if (container) {
-    ReactDOM.createRoot(container).render(<App config={config} />)
+  if (!container) {
+    throw new Error(`Element with id "${containerId}" not found`)
+  }
+
+  const iframe = document.createElement('iframe')
+  iframe.style.border = 'none'
+
+  // todo - use iframe.src instead of iframe.srcdoc
+  // iframe.src = './widget.html'
+  iframe.srcdoc = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Widget</title>
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+  </html>
+`
+
+  container.appendChild(iframe)
+
+  iframe.onload = () => {
+    console.log('iframe.onload')
+    const doc = iframe.contentDocument || iframe.contentWindow?.document
+    if (!doc) {
+      throw new Error('Failed to access iFrame document')
+    }
+
+    const appContainer = doc.getElementsByTagName('body')
+
+    if (appContainer[0]) {
+      ReactDOM.createRoot(appContainer[0]).render(<App config={config} />)
+    } else {
+      throw new Error('iframe body not found')
+    }
   }
 }
