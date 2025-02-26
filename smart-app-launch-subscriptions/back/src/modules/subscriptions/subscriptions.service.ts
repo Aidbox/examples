99eac602c0bd596d750c451d12dd74ae2bf56a3f
Igor Kisliakov <igor_kislyakov@yahoo.com>
2025-02-26 16:12:14 +0300

    smart app subscriptions - front - added modules integration

--- smart-app-launch-subscriptions/back/src/modules/subscriptions/subscriptions.service.ts
+++ smart-app-launch-subscriptions/back/src/modules/subscriptions/subscriptions.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common'
 import { SubscriptionsDto } from './dto/subscriptions.dto'
+import { EventsService } from '../events/events.service'
 
 @Injectable()
 export class SubscriptionsService {
+
+  constructor(
+    private readonly eventsService: EventsService
+  ) { }
+
   async postAllNewSubscriptionEvents(payload: SubscriptionsDto) {
     console.log('postAllNewSubscriptionEvents:')
     console.log(payload)
+
+    this.eventsService.sendMessage({
+      userId: 'asd', // doctor's id in our case,
+      date: new Date().toISOString(),
+      msg: payload ? JSON.stringify(payload) : ''
+    })
+
     return []
   }
 }
