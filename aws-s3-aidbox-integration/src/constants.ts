51cc8e2ac0b5cbef8b7861478633b16f3092c653
spicyfalafel <58147555+spicyfalafel@users.noreply.github.com>
2025-02-26 13:56:26 +0000

    Deploying to gh-pages from @ Aidbox/examples@69553fc25d0cdcf4113ebee7a5d17c64a4511a7e 🚀

--- aws-s3-aidbox-integration/src/constants.ts
+++ aws-s3-aidbox-integration/src/constants.ts
@@ -1,3 +1,3 @@
-export const aidboxAWSBucketUrl = "http://localhost:8888/aws/storage/my-account/csv-load-from-bucket";
+export const aidboxAWSBucketUrl = "http://localhost:8888/aws/storage/my-account/mybucket";
 export const aidboxUrl = "http://localhost:8888/fhir";
 export const basicAuthHeader = "Basic YmFzaWM6c2VjcmV0";
