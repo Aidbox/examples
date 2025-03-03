43a9031acf923bab30d7a0223cdc8b3df9875ba6
Igor Kisliakov <igor_kislyakov@yahoo.com>
2025-03-03 18:08:55 +0300

    smart app subscriptions - example - fixed docker configs

--- smart-app-launch-subscriptions/example-react-ehr/src/main.tsx
+++ smart-app-launch-subscriptions/example-react-ehr/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '../../front/dist/smart-app-launch.subscriptions.umd.js'
+// import '../../front/dist/smart-app-launch.subscriptions.umd.js'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
