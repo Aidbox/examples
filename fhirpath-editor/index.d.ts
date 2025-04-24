31a80e667aa6b24471a4e74974f31446bdd730bf
olimsaidov <olim.mail@gmail.com>
2025-04-24 14:26:47 +0000

    Deploying to gh-pages from @ Aidbox/examples@9226fb75992cb05229783d3633a1ed4dd1c3a0d8 🚀

---
+++ fhirpath-editor/index.d.ts
@@ -0,0 +1,42 @@
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+
+export declare function Editor({ defaultValue, onChange, data, variables, schema, debug, }: IEditorProps): JSX_2.Element;
+
+declare type FhirElement = {
+    array?: boolean;
+    scalar?: boolean;
+    min?: number;
+    max?: number;
+    choiceOf?: string;
+    choices?: string[];
+    excluded?: string[];
+    required?: string[];
+    elementReference?: string[];
+    type?: string;
+    elements?: FhirElements;
+};
+
+declare type FhirElements = Record<string, FhirElement>;
+
+export declare type FhirSchema = {
+    id: string;
+    url: string;
+    type: string;
+    derivation: "specialization" | "constraint";
+    kind: "complex-type" | "logical" | "primitive-type" | "resource";
+    base?: string;
+    excluded?: string[];
+    required?: string[];
+    elements?: FhirElements;
+};
+
+declare interface IEditorProps {
+    defaultValue: string;
+    onChange?: (value: string) => void;
+    data: any;
+    variables?: Record<string, any>;
+    schema: FhirSchema[];
+    debug?: boolean;
+}
+
+export { }
