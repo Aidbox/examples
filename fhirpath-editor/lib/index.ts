d2a1885bc0e945fe54f216a9cee359d2726d2248
Olim Saidov <olim.mail@gmail.com>
2025-06-20 19:38:27 +0500

    Add redirect from original GitHub Pages URL to new repository
    - Replace fhirpath-editor build with HTML redirect page
    - Redirect users from aidbox.github.io/examples/fhirpath-editor/ to healthsamurai.github.io/fhirpath-editor/
    - Include countdown timer and styled redirect page for better UX

--- fhirpath-editor/lib/index.ts
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-import { style } from "./style.tsx";
-import { text } from "./text.tsx";
-
-export { default as Editor } from "./Editor";
-export type { FhirSchema } from "./types/internal";
-export { style, text };
